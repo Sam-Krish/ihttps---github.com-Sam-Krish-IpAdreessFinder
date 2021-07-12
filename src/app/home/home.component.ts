833d29f3a928588b0ed436b8a7cded51c1eaaf2e
skarthiveemak <skarthi@veemak.com>
2021-07-12 21:07:43 +0530

    master

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -65,6 +65,7 @@ console.log(latitude);
 console.log(longitude)
 
 let mymap = L.map('map').setView([longitude,latitude], 13);
+console.log(mymap)
 
 L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
